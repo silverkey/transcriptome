a4e9730584af5250ee11484780a455494f3c4e87
remo <remo@Z800.szn>
2013-03-07 16:17:40 +0100

    updated filter_fasta to make it to create also a file with sequences that did not pass the filter because not beeing the longest isoform

--- filter_assembly.pl
+++ filter_assembly.pl
@@ -1,7 +1,10 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use Data::Dumper;
 use Bio::SeqIO;
+
+# VERSION: 2.0
 
 my $fasta = $ARGV[0];
 my $length = $ARGV[1];
@@ -15,7 +18,12 @@ my $new = "$fasta";
 $new =~ s/\.(.+)$//;
 $new .= "_comp_length_$length\.fasta";
 
+my $nopass = "$new";
+$nopass =~ s/\.fasta$/_NOPASS.fasta/;
+
 my $href = {};
+my $lengthok = {};
+my $written = {};
 
 my $in = Bio::SeqIO->new(-file => $fasta,
                          -format => 'fasta');
@@ -23,10 +31,14 @@ my $in = Bio::SeqIO->new(-file => $fasta,
 my $out = Bio::SeqIO->new(-file => ">$new",
                           -format => 'fasta');
 
+my $ref = Bio::SeqIO->new(-file => ">$nopass",
+                          -format => 'fasta');
+
 while(my $seq = $in->next_seq) {
   next unless $seq->length >= $length;
   my $length = $seq->length;
   my $id = $seq->id;
+  $lengthok->{$id} = $seq;
   my(@val) = split(/\_/,$id);
   my $compc = join('_',($val[0],$val[1]));
   my $at = $val[2];
@@ -42,6 +54,11 @@ while(my $seq = $in->next_seq) {
 }
 
 foreach my $compc(keys %$href) {
+  $written->{$href->{$compc}->id} ++;
   $out->write_seq($href->{$compc});
 }
 
+foreach my $id(keys %$lengthok) {
+  next if exists $written->{$id};
+  $ref->write_seq($lengthok->{$id});
+}
